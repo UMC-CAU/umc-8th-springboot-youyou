198f540fa2019627e23de97aa902fe345258a8b9
kite_U <yurry45@naver.com>
2025-05-07 16:05:53 +0900

    feature/#3: Region Entity 생성

--- src/main/java/com/umc/bios/domain/store/entity/Region.java
+++ src/main/java/com/umc/bios/domain/store/entity/Region.java
@@ -1,4 +1,16 @@
 package com.umc.bios.domain.store.entity;
 
-public class Region {
+import com.umc.bios.domain.common.BaseEntity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "region")
+public class Region extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "region_id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
 }
