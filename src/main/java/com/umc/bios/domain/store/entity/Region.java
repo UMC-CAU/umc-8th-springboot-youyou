e24b1d626395ac009a83a5e94285f583b1f1a439
kite_U <yurry45@naver.com>
2025-05-07 21:44:40 +0900

    feature/#3: Region Getter 어노테이션 추가

--- src/main/java/com/umc/bios/domain/store/entity/Region.java
+++ src/main/java/com/umc/bios/domain/store/entity/Region.java
@@ -2,9 +2,11 @@ package com.umc.bios.domain.store.entity;
 
 import com.umc.bios.domain.common.BaseEntity;
 import jakarta.persistence.*;
+import lombok.Getter;
 
 @Entity
 @Table(name = "region")
+@Getter
 public class Region extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
