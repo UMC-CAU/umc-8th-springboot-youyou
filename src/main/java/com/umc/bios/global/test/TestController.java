6483cae722c8275e776a7bd900fc5494a7e8cb63
kite_U <yurry45@naver.com>
2025-05-13 12:34:42 +0900

    refactor/#6: 응답 테스트를 위한 TestController 구현

--- src/main/java/com/umc/bios/global/test/TestController.java
+++ src/main/java/com/umc/bios/global/test/TestController.java
@@ -1,6 +1,11 @@
 package com.umc.bios.global.test;
 
+import com.umc.bios.global.response.CommonResponse;
+import com.umc.bios.global.response.ErrorStatus;
+import com.umc.bios.global.response.SuccessStatus;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/test")
 @RequiredArgsConstructor
+@Tag(name = "응답 테스트 API")
 public class TestController {
 
     private final HelloService helloService;
@@ -17,5 +23,22 @@ public class TestController {
         return helloService.sayHello();
     }
 
+    @GetMapping("/no-data")
+    public ResponseEntity<CommonResponse<?>> testOnSuccess() {
+        return ResponseEntity.ok(CommonResponse.of(SuccessStatus._OK));
+    }
 
+    @GetMapping("/data")
+    public ResponseEntity<CommonResponse<?>> testOnSuccessWithData() {
+        return ResponseEntity.ok(
+                CommonResponse.of(SuccessStatus._OK, "Good")
+        );
+    }
+
+    @GetMapping
+    public ResponseEntity<?> testOnFailure() {
+        return ResponseEntity.ok(
+                CommonResponse.onFailure(ErrorStatus.MEMBER_NOT_FOUND, null)
+        );
+    }
 }
