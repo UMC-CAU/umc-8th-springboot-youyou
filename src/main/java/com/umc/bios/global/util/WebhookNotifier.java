f0cd47fa5df0dd5df6527f1f5eeb6c90922c3c29
kite_U <yurry45@naver.com>
2025-05-27 01:56:16 +0900

    chore/#10: .gitkeep 삭제

--- src/main/java/com/umc/bios/global/util/WebhookNotifier.java
+++ src/main/java/com/umc/bios/global/util/WebhookNotifier.java
@@ -12,27 +12,27 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
-@Component
-@RequiredArgsConstructor
-@Slf4j
-@Profile("dev")
-public class WebhookNotifier {
-
-    @Value("${webhook.discore-url}")
-    private String discordWebhookUrl;
-
-    private final RestTemplate restTemplate = new RestTemplate();
-
-    public void send(String message) {
-        if (!StringUtils.hasText(discordWebhookUrl)) return;
-
-        Map<String, String> payload = Map.of("content", message);
-
-        try {
-            restTemplate.postForEntity(discordWebhookUrl, payload, String.class);
-        } catch (Exception e) {
-            log.warn("웹훅 전송 실패: {} ", e.getMessage());
-            throw CustomException.of(ErrorStatus.FAILED_SEND_WEBHOOK);
-        }
-    }
-}
+//@Component
+//@RequiredArgsConstructor
+//@Slf4j
+//@Profile("dev")
+//public class WebhookNotifier {
+//
+//    @Value("${webhook.discord-url}")
+//    private String discordWebhookUrl;
+//
+//    private final RestTemplate restTemplate = new RestTemplate();
+//
+//    public void send(String message) {
+//        if (!StringUtils.hasText(discordWebhookUrl)) return;
+//
+//        Map<String, String> payload = Map.of("content", message);
+//
+//        try {
+//            restTemplate.postForEntity(discordWebhookUrl, payload, String.class);
+//        } catch (Exception e) {
+//            log.warn("웹훅 전송 실패: {} ", e.getMessage());
+//            throw CustomException.of(ErrorStatus.FAILED_SEND_WEBHOOK);
+//        }
+//    }
+//}
